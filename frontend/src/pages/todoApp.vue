0c9ef19b68702cf4a56bebaeecaf20b60994ff30
Trinmar Boado <timi.boado@gmail.com>
2022-08-21 15:25:54 +0800

    traditional html

---
+++ frontend/src/pages/todoApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <h6>Hello Batch 43 Sir Jun</h6>
+  <input type="text">
+  <ul>
+    <li>
+      <input type="checkbox" />
+      sdfsdfsd sdf
+      <button>x</button>
+    </li>
+    <li>
+      <input type="checkbox" />
+      sdfsdfsd sdf
+      <button>x</button>
+    </li>
+  </ul>
+</template>
