b600b01f4a8e3aeef07b4bd9cfc26b862488f1b4
Trinmar Boado <timi.boado@gmail.com>
2022-08-21 15:19:28 +0800

    addPath to todoApp.vue

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -7,6 +7,10 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: '/todos',
+    component: () => import('pages/todoApp.vue')
+  },
 
   // Always leave this as last one,
   // but you can also remove it
