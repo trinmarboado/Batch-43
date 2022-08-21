0fafbc22dcc2078bbbc0d3309cd7fe15df81b070
Trinmar Boado <timi.boado@gmail.com>
2022-08-21 16:30:40 +0800

    convert html tags to quasar components

--- frontend/src/pages/todoApp.vue
+++ frontend/src/pages/todoApp.vue
@@ -1,18 +1,32 @@
 <template>
-  <h6 class="title" id="title">Hello Batch 43 Sir Jun</h6>
-  <input type="text">
-  <ul>
-    <li>
-      <input type="checkbox" />
-      sdfsdfsd sdf
-      <button>x</button>
-    </li>
-    <li>
-      <input type="checkbox" />
-      sdfsdfsd sdf
-      <button>x</button>
-    </li>
-  </ul>
+  <q-toolbar class="bg-primary text-white">
+    <q-btn flat round dense icon="menu" class="q-mr-sm" />
+    <q-avatar>
+      <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+    </q-avatar>
+
+    <q-toolbar-title>Batch 43 Todo App</q-toolbar-title>
+
+    <q-btn flat round dense icon="whatshot" />
+  </q-toolbar>
+  <q-input filled modelValue="" label="Filled" />
+  <q-list bordered separator>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon name="signal_wifi_off" />
+      </q-item-section>
+      <q-item-section>Active</q-item-section>
+      <q-item-section side>Side</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <q-icon name="signal_wifi_off" />
+      </q-item-section>
+      <q-item-section>Active</q-item-section>
+      <q-item-section side>Side</q-item-section>
+    </q-item>
+  </q-list>
+
 </template>
 
 <style>
